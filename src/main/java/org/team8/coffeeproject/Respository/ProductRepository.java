41610281d20a3a77877d3ec8a0047d55913a853b
HYEONSIK <ks06891@naver.com>
2024-12-06 11:41:13 +0900

    refactor: Boot JPA -> JPA로 리펙토링 2차

--- src/main/java/org/team8/coffeeproject/Respository/ProductRepository.java
+++ src/main/java/org/team8/coffeeproject/Respository/ProductRepository.java
@@ -1,6 +1,8 @@
 package org.team8.coffeeproject.Respository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import jakarta.persistence.EntityManager;
+
+import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import org.team8.coffeeproject.Entity.Product;
 
@@ -8,9 +10,24 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    Product findByProductNm(String productNm);
-    Optional<Product> findById(Long productId);
-    List<Product> findAll();
+public class ProductRepository {
+    @PersistenceContext
+    private EntityManager em;
 
+    public Optional<Product> findById(Long productId){
+
+        Optional<Product> product = Optional.ofNullable(em.createQuery("select p from Product p where p.id = :productId", Product.class)
+                        .setParameter("productId", productId)
+                        .getSingleResult());
+
+        return product;
+    };
+
+    public List<Product> findAll() {
+
+        List<Product> productList = em.createQuery("select p from Product p", Product.class)
+                                      .getResultList();
+
+        return productList;
+    }
 }
